b92eeb144573ce9e36f9f92ff690aec7f5bac31c
DENNIS MUUO <dennismuuo.dev@gmail.com>
2025-11-30 17:21:44 +0300

    Changed logo height

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ export function Header() {
             <img
               src="/logo.png"
               alt="Logo"
-              className="w-16 h-16 object-contain"
+              className="w-10 h-10 object-contain"
             />
 
           </Link>
